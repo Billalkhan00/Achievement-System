8293f7ee9f039e3d2255138ea71395bbcf7624ae
Billalkhan00 <billalkhan00@hotmail.com>
2023-03-27 00:15:07 +0100

    header and footer

--- index.php
+++ index.php
@@ -1,1 +1,22 @@
-<html></html>
+<html>
+
+
+
+    <head>
+        <meta charset="utf-8">
+        <meta name="Author" content="Billal Khan">
+        <meta name="Description" content="High Achievers website for School children">
+        <title>High Achievers</title>
+        <link rel="stylesheet" type="text/css" href="index.css">
+    </head>
+
+
+
+    <body>
+    
+
+    </body>
+
+
+
+</html>
---
+++ php/header.php
@@ -0,0 +1,11 @@
+<div>
+    <h1>High Achievers</h1>
+    <div>
+        <a href="#">Home</a>
+        <a href="#">About</a>
+        <a href="#">Achievements</a>
+        <a href="#">Leaderboard</a>
+        <a href="#">Signup</a>
+        <a href="#">Login</a>
+    </div>
+</div>
