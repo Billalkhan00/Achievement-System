fcdf4d5807f309d038bf668207c1e043f99c9328
Billalkhan00 <billalkhan00@hotmail.com>
2023-04-07 00:49:40 +0100

    Student Homepage
    I made the student homepage. I made this so each student is able to visit there homepage with all there personal details on it.

---
+++ studenthomepage.php
@@ -0,0 +1,40 @@
+<html>
+
+
+
+    <head>
+        <meta charset="utf-8">
+        <meta name="Author" content="Billal Khan">
+        <meta name="Description" content="High Achievers website for School children">
+        <title>High Achievers</title>
+        <link rel="stylesheet" type="text/css" href="index.css">
+    </head>
+
+
+
+    <body>
+    <?php
+    include("php/header.php");
+    ?>
+    <h1>Student Homepage</h1>
+    <?php
+    include "php/database.php";
+    if (isset($_SESSION["user"])==FALSE){
+        header("location: studentloginpage.php");
+        exit();
+    }
+    $userID = $_SESSION ["user"] -> ChildID; 
+    $user = $_SESSION ["user"];
+    ?>
+    <p> <?php echo $user ->First_Name . " " . $user -> Last_Name ?></p>
+    <p> <?php echo $user ->Email_Address?> </p>
+    <p> <?php echo $user ->Class?> </p>
+    <p> <?php echo $user ->Date_Of_Birth?> </p>
+    <?php
+    include("php/footer.php");
+    ?>
+    </body>
+
+
+
+</html>
